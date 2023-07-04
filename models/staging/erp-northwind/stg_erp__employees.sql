54ceb23ae6932aa963d8ff504e332da5813b777b
yvestidor.eth <yvesh2603@gmail.com>
2023-07-04 20:37:29 +0000

    Finalizada dim_employees

--- models/staging/erp-northwind/stg_erp__employees.sql
+++ models/staging/erp-northwind/stg_erp__employees.sql
@@ -1,7 +1,8 @@
 with
     source_employees as (
         select 
-            cast(employee_id as int) as funcionario_id					
+            cast(employee_id as int) as funcionario_id
+            , cast(reports_to as int) gerente_id					
             , cast(last_name as string)	as sobrenome					
             , cast(first_name as string) as nome
             , cast(first_name || ' ' || last_name as string) as nome_completo						
@@ -17,8 +18,7 @@ with
             --, home_phone						
             --, extension						
             --, photo						
-            , notes	as notas			
-            , reports_to as gerente_id					
+            , notes	as notas				
             --, photo_path			
         from {{ source('erp', 'employees') }}
     )
